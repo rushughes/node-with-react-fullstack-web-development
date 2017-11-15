8fb81fb58930ea2bc986b5030077492cee97fc9a
Rus Hughes <russell.hughes@gmail.com>
2017-11-15 16:43:23 +0700

    lecture 81

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'im logged out';
+      default:
+        return 'im logged in';
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
