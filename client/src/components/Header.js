a912e53b7c107cfce3a2819dac97cfdf5b087007
Rus Hughes <russell.hughes@gmail.com>
2017-11-15 20:38:26 +0700

    lecture 95

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,11 +17,11 @@ class Header extends Component {
         );
       default:
         return [
-          <li>
+          <li key="1">
             <Payments />
           </li>,
 
-          <li>
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
