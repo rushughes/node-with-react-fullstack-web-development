f9c867e33fa13b3aa991231fcedba72193d3fb17
Rus Hughes <russell.hughes@gmail.com>
2017-11-15 13:13:56 +0700

    lecture 58

--- services/passport.js
+++ services/passport.js
@@ -23,23 +23,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile.id
-          console.log('User already exists with googleId:', profile.id);
-          // we tell passport that we are done and pass in the User object
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this id so make a new record
-          console.log('Creating new User googleId:', profile.id);
-          new User({ googleId: profile.id })
-            // we save the user
-            .save()
-            // we tell passport that we are done and pass in the User object
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
