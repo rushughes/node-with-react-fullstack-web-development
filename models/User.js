039537d5fffba337ae1f5f21e000a9ae8b1e2ebf
Rus Hughes <russell.hughes@gmail.com>
2017-11-16 14:20:41 +0700

    lecture 103

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
-  credits: { type: number, default: 0 },
+  credits: { type: Number, default: 0 },
 });
 
 mongoose.model('users', userSchema);
