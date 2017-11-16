8242e07b8bab3abe8450db6cca392678f0c47055
Rus Hughes <russell.hughes@gmail.com>
2017-11-16 19:01:26 +0700

    lecture 109

--- index.js
+++ index.js
@@ -31,5 +31,18 @@ app.use(function(req, res, next) {
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV == 'production') {
+  // express will serve up static production assets
+  // main.js main.css etc
+  app.use(express.static('client/build'));
+
+  // express will serve up the index.html if it does
+  // not recognise the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
