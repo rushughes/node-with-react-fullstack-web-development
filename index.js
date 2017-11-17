4f7cba998a5619bda47b71e8c295481b3f498d65
Rus Hughes <russell.hughes@gmail.com>
2017-11-17 19:33:54 +0700

    lecture 120

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(function(req, res, next) {
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV == 'production') {
   // express will serve up static production assets
