313062a698d4e395edd7af128ab95c95f4ddd822
Rus Hughes <russell.hughes@gmail.com>
2017-11-17 21:13:52 +0700

    lecture 130

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,13 @@ const sendGrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
