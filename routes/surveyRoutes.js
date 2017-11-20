a5a2477f0e8f6ac68a5dc1afcf8f8a9f6e99e373
Rus Hughes <russell.hughes@gmail.com>
2017-11-20 18:23:46 +0700

    lecture 185

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -30,7 +30,7 @@ module.exports = app => {
     // }];
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -43,9 +43,22 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+          }
+        ).exec();
+      })
       .value();
 
-    console.log(events);
     res.send({});
   });
 
