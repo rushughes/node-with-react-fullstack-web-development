b7330da26a73404186d051b98a2d97709cbe718d
Rus Hughes <russell.hughes@gmail.com>
2017-11-20 17:24:50 +0700

    lecture 179

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -40,7 +40,9 @@ module.exports = app => {
         };
       }
     });
-    console.log(events);
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log(uniqueEvents);
     res.send({});
   });
 
