8809f708dbc221826ae9035799c668a6f25518c1
Rus Hughes <russell.hughes@gmail.com>
2017-11-20 13:01:31 +0700

    lecture 163

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -18,8 +19,17 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn=flat" onClick={onCancel}>
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        className="green white-text btn-flat right"
+        onClick={() => submitSurvey(formValues)}
+      >
+        Send Survey<i className="material-icons right">email</i>
       </button>
     </div>
   );
